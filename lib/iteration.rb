1ad0e7c6953f9b30f6fadf59b860363f33417c54
Robert Phillips <rpdecks@gmail.com>
2020-03-13 23:57:54 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -47,7 +47,7 @@ def total_even_pairs(src)
   element_index = 0
   while element_index < src.length do
     if modulo(src[element_index][0]) + modulo(src[element_index][1]) == 0
-    total += src[element_index][0]
+    total += (src[element_index][0] += src[element_index][1])
     else
     results << src[element_index][1]
     end
