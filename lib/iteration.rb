212b72745e0e597708bdabda9015dd7ca4dee1b4
Robert Phillips <rpdecks@gmail.com>
2020-03-13 23:40:20 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,7 +24,7 @@ def find_greater_pair(src)
   results = []
   element_index = 0
   while element_index < src.length do
-    results << max
+    results << max()
     element_index += 1
   end
   results
