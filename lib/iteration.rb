2d362bcb256bd56378228bd23af7c91aa7d2953c
Robert Phillips <rpdecks@gmail.com>
2020-03-13 23:31:27 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -10,7 +10,7 @@ def join_ingredients(src)
   results = []
   element_index = 0
   while row_index < src.length do
-    results[element_index] << "I love %{src[element_index][0]} and `src[element_index][1]` on my pizza`
+    results[element_index] << I love %{src[element_index][0]} and %src[element_index][1] on my pizza
     element_index += 1
   end
   results
