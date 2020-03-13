80a3b0e90041bde1581637aa5601d81720953032
Robert Phillips <rpdecks@gmail.com>
2020-03-13 23:11:06 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -8,7 +8,7 @@ def join_ingredients(src)
   # As such, there should be a new String for each inner array, or pair
   
   outer_results = []
-  row_index = []
+  row_index = 0
   while row_index < src.count do
     
 end
