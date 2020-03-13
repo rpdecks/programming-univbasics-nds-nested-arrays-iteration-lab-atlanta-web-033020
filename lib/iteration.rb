8f4a0f4ad7efc704f9e52b5f141c11083b93cd97
Robert Phillips <rpdecks@gmail.com>
2020-03-13 23:12:27 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -12,7 +12,7 @@ def join_ingredients(src)
   while row_index < src.count do
     element_index = 0
     inner_results = []
-    while element_index < src[row_index]
+    while element_index < src[row_index].count do
 end
 
 def find_greater_pair(src)
